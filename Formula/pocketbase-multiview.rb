efb94643af50160ad34901317e676d918c3ab56a
jiseop121 <ghdwltjq5749@naver.com>
2026-03-02 01:52:49 +0900

    chore(formula): release pocketbase-multiview v0.2.1

--- Formula/pocketbase-multiview.rb
+++ Formula/pocketbase-multiview.rb
@@ -6,10 +6,10 @@ class PocketbaseMultiview < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/jiseop121/multi-pocketbase-ui/releases/download/v0.2.1/pbmulti-v0.2.1-darwin-arm64.tar.gz"
-      sha256 "60d412ef660e931f7910a95a62e9422279bd279b0d8f9d2e58e1d213e43b19b9"
+      sha256 "b8b50e7b184eeb401128c91c6eca548201407d7b228de218b23c6e3ab95ea92b"
     else
       url "https://github.com/jiseop121/multi-pocketbase-ui/releases/download/v0.2.1/pbmulti-v0.2.1-darwin-amd64.tar.gz"
-      sha256 "230cfd17739ade43085c969e6a8a66b478eebd994ac4649cf5c35e8e83dab3ec"
+      sha256 "79b2b8bd6015c0b3e19022a546e980ed1db84bba4f53afbee57ceb5f929a1d37"
     end
   end
 
